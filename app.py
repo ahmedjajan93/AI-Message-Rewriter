118f23b06c2c310af8f5ad45901d5ef441c3ac8d
Ahmad Jajan <65482039+ahmedjajan93@users.noreply.github.com>
2025-04-15 07:40:57 -0700

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ if not api_key:
 os.environ["OPENAI_API_KEY"] = api_key
 
 models = {
-    "mistralai/mistral-small-3.1-24b-instruct:free": "Mistral Small 3.1",
+    "openai/gpt-3.5-turbo:free": "gpt-3.5",
     "deepseek/deepseek-chat:free": "deepseek-chat",
     "meta-llama/llama-4-maverick:free": "llama-4-maverick",
     "google/gemini-2.0-flash-thinking-exp:free": "Gemini 2.0 Flash Thinking",
